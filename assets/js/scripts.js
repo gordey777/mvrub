a25b8e2656b3ff9b2f43cf0e663d2c01c44dbe47
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2016-11-01 15:36:33 +0200

    move scripts from html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,3 +21,101 @@ if (typeof jQuery === 'undefined') {
   console.log('jQuery has loaded');
 }
 // Place any jQuery/helper plugins in here.
+
+
+//TABS
+
+  (function($) {
+    jQuery.fn.lightTabs = function(options) {
+
+      var createTabs = function() {
+        tabs = this;
+        i = 0;
+
+        showPage = function(i) {
+          $(tabs).children("div").children("div").hide();
+          $(tabs).children("div").children("div").eq(i).show();
+          $(tabs).children("ul").children("li").removeClass("active");
+          $(tabs).children("ul").children("li").eq(i).addClass("active");
+        }
+
+        showPage(0);
+
+        $(tabs).children("ul").children("li").each(function(index, element) {
+          $(element).attr("data-page", i);
+          i++;
+        });
+
+        $(tabs).children("ul").children("li").click(function() {
+          showPage(parseInt($(this).attr("data-page")));
+        });
+      };
+      return this.each(createTabs);
+    };
+  })(jQuery);
+    $(document).ready(function() {
+    $(".tabs").lightTabs();
+  });
+
+//SCROLL
+
+    $headH = $('#header').height() ;
+      //alert($headH );
+    $("#section-1").css("margin-top", $headH + "px");
+
+
+    (function($){
+      $(window).load(function(){
+
+        $("#nav-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+        highlightSelector:"#nav-menu a"
+        });
+
+        $("a[rel='next']").click(function(e){
+        e.preventDefault();
+        var to=$(this).parent().parent("section").next().attr("id");
+        $.mPageScroll2id("scrollTo",to);
+        });
+
+      });
+    })(jQuery);
+
+    (function($){
+      $(window).load(function(){
+
+      $("#nav-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+
+              scrollSpeed:1300,
+              autoScrollSpeed:true,
+              pageEndSmoothScroll:true,
+              offset: $headH,
+              forceSingleHighlight:false,
+              highlightByNextTarget:false,
+              /* disable plugin below [x,y] screen size: boolean, integer, array ([x,y]) */
+              disablePluginBelow:false,
+        });
+      });
+    })(jQuery);
+
+// YMAPS
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.931011, 30.359388],
+            zoom: 18
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: '',
+            balloonContent: ''
+        }, {
+
+            iconLayout: 'default#image',
+            iconImageHref: '../img/icon-map.png',
+            iconImageSize: [100, 130],
+            iconImageOffset: [-50, -130]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
+
